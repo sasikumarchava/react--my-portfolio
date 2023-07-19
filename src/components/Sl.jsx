f85414628152df54be5c018254d1b7e282dd3e80
SASI CHAVA <88151734+sasikumarchava@users.noreply.github.com>
2023-07-19 21:33:53 +0530

    Update Sl.jsx

--- src/components/Sl.jsx
+++ src/components/Sl.jsx
@@ -62,7 +62,7 @@ const Sl = () => {
 
             <li key={id} 
                 className={
-                'flex justify-between items-center w-40 h-10 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500'+ ""+ style} >
+                'flex justify-between items-center w-40 h-10 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500'+ style} >
             <a href={href} className='flex justify-between items-center w-full text-white' download={download} target='_blank' rel='noreferrer'>
              
              {child}
